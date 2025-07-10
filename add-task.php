6e580e2f810c32ee4aba45e2724bcb90ee18f990
Brouev <c.charlot333@gmail.com>
2025-07-10 22:15:16 +0200

    Update add-task.php

--- add-task.php
+++ add-task.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['user_id'])) {
 require 'pdo.php';
 if ($_SERVER['REQUEST_METHOD']==='POST'&&!empty($_POST['task_name'])){
     $stmt=$pdo->prepare ("INSERT INTO tasks (user_id, task_name, task_details)VALUES(?,?,?)");
-    $stmt->execute([$_SESSION ['user_id'], $_POST ['task_name'], $_POST ['details']??"]);
+    $stmt->execute([$_SESSION ['user_id'], $_POST ['task_name'], $_POST ['details']?? null]);
 }
 header('Location: tasklist.php');
 exit;
